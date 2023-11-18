7c026f13325a604168419071efa38ea7c4245fa0
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2023-11-18 20:31:16 +0100

    `fix` : HelloApplication.java remove mysql conn test

--- src/main/java/com/chatapp/presentation/views/HelloApplication.java
+++ src/main/java/com/chatapp/presentation/views/HelloApplication.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.sql.*;
 
 import java.io.IOException;
 
@@ -16,19 +15,6 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-        System.out.println("test");
-        String url = "jdbc:mysql://localhost:3306/projet-web";
-        String username = "root";
-        String password = "";
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection = DriverManager.getConnection(url, username, password);
-            System.out.println("Connected to MySQL database!");
-        } catch (SQLException e) {
-            System.out.println("Connection failed. Error: " + e.getMessage());
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     public static void main(String[] args) {
