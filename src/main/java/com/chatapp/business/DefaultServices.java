5e03e642b138466e14fe4d0a5733dc938d462b13
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2023-11-19 10:30:40 +0100

    `feat` : DefaultServices's implementation using singleton added

--- src/main/java/com/chatapp/business/DefaultServices.java
+++ src/main/java/com/chatapp/business/DefaultServices.java
@@ -1,4 +1,21 @@
 package com.chatapp.business;
 
 public class DefaultServices implements IServices {
+
+    private static DefaultServices instance = null;
+
+
+    private DefaultServices() {
+
+    }
+
+    public static DefaultServices getInstance(){
+
+        if(instance == null)
+            instance = new DefaultServices();
+
+        return instance;
+
+    }
+
 }
