b6b60977d556777b50a80af4d12548951a71aae1
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2023-11-19 19:05:58 +0100

    `feat` : add encrypt and decrypt methods

--- src/main/java/com/chatapp/beans/Message.java
+++ src/main/java/com/chatapp/beans/Message.java
@@ -1,11 +1,25 @@
 package com.chatapp.beans;
 
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
 public class Message {
     private User sender, reciever ;
+    private String content;
 
-    public Message(User sender, User receiver) {
+    public Message(User sender, User receiver,String content) {
         this.sender = sender;
         this.reciever = receiver;
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 
     public User getSender() {
@@ -14,5 +28,28 @@ public class Message {
 
     public User getReciever() {
         return reciever;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public void setReciever(User reciever) {
+        this.reciever = reciever;
+    }
+
+    public static String encryptMessageContent(String content, SecretKey secretKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedBytes = cipher.doFinal(content.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    public static String decryptMessageContent(String encryptedContent, SecretKey secretKey) throws Exception {
+        byte[] encryptedBytes = Base64.getDecoder().decode(encryptedContent);
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+        return new String(decryptedBytes);
     }
 }
