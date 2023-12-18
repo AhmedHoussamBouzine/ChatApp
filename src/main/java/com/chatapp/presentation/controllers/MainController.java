f21d90b4569c065ffa942366c45a09ca029cb7b7
ayoub adil <55322505+ayoubad12@users.noreply.github.com>
2023-12-19 00:51:38 +0100

    made the main interface

--- src/main/java/com/chatapp/presentation/controllers/MainController.java
+++ src/main/java/com/chatapp/presentation/controllers/MainController.java
@@ -3,7 +3,7 @@ package com.chatapp.presentation.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
