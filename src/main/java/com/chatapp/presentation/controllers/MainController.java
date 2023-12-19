05b88955a0df7df501b8e11cb0e8fbe54b2a6471
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2023-12-20 00:47:36 +0100

    `fix` : fix gluon problem

--- src/main/java/com/chatapp/presentation/controllers/MainController.java
+++ src/main/java/com/chatapp/presentation/controllers/MainController.java
@@ -2,6 +2,7 @@ package com.chatapp.presentation.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class MainController {
     @FXML
